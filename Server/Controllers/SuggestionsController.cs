7fcb126e63aa818b1a959787bc491a32549aae57
Tobias Kroknes Kristiansen <tobias.kristiansen@norsktitanium.com>
2022-10-18 11:43:30 +0200

    Merge and small changes to front end

--- Server/Controllers/SuggestionsController.cs
+++ Server/Controllers/SuggestionsController.cs
@@ -1,19 +1,141 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Nordic_Door.Server.Data;
+using Nordic_Door.Shared.Models.API;
+using Nordic_Door.Shared.Models.Database;
+using static MudBlazor.Colors;
 
 namespace Nordic_Door.Server.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class SuggestionsController : Controller
-{
+    public class SuggestionController : Controller
+    {
         public NordicDoorsDbContext dbContext { get; set; }
 
-        public SuggestionsController(NordicDoorsDbContext ctx)
+        public SuggestionController(NordicDoorsDbContext ctx)
         {
             dbContext = ctx;
         }
 
+        [HttpGet]
+        [Route("/Search/All/Suggestion")]
+        public async Task<IActionResult> GetSuggestions()
+        {
+            var suggestion = await dbContext.Suggestions.ToListAsync();
+            return Ok(suggestion);
+        }
 
+        [HttpGet]
+        [Route("/Search/SuggestionById{id:int}")]
+        public async Task<IActionResult> GetSuggestionById([FromRoute] int id)
+        {
+            var suggestion = await dbContext.Suggestions.FindAsync(id);
+
+            if (suggestion == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(suggestion);
+        }
+
+        [HttpGet]
+        [Route("/Search/Firstby/{title}")]
+        public async Task<IActionResult> GetFirstSuggestionByTitle([FromRoute] string title)
+        {
+
+            var suggestion = await dbContext.Suggestions.FirstAsync(suggestion => suggestion.Title == title);
+
+            if (suggestion == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(suggestion);
+        }
+
+
+        [HttpGet]
+        [Route("/Search/All/Suggestion/{title}")]
+        public async Task<IActionResult> GetAllSuggestionsByTitle([FromRoute] string title)
+        {
+
+            var suggestion = await dbContext.Suggestions.Where( suggestion => suggestion.Title  == title).ToListAsync();
+
+            if ( suggestion == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(suggestion);
+        }
+
+
+        [HttpPut]
+        [Route("/Update/Suggestion/{id:int}")]
+        public async Task<IActionResult> UpdateUser([FromRoute] int id, UpdateSuggestionRequest updateSuggestionRequest)
+        {
+            var suggestion = await dbContext.Suggestions.FindAsync(id);
+
+            if (suggestion != null)
+            {
+                suggestion.TeamId = updateSuggestionRequest.TeamId;
+                suggestion.ResponsibleEmployee = updateSuggestionRequest.ResponsibleEmployee;
+                suggestion.ResponsibleTeam = updateSuggestionRequest.ResponsibleTeam;
+                suggestion.Title = updateSuggestionRequest.Title;
+                suggestion.DeadLine = updateSuggestionRequest.DeadLine;
+                suggestion.Phase = updateSuggestionRequest.Phase;
+                suggestion.Description = updateSuggestionRequest.Description;
+
+                await dbContext.SaveChangesAsync();
+
+                return Ok(suggestion);
+            }
+            return NotFound();
+        }
+
+
+        [HttpPost]
+        [Route("/Add/Suggestion")]
+        public async Task<IActionResult> AddSuggestion(AddSuggestionRequest addSuggestionRequest)
+        {
+            var suggestion = new Suggestion()
+            {
+
+                TeamId = addSuggestionRequest.TeamId,
+                CreatedBy = addSuggestionRequest.CreatedBy,
+                ResponsibleEmployee = addSuggestionRequest.ResponsibleEmployee,
+                ResponsibleTeam = addSuggestionRequest.ResponsibleTeam,
+                Title = addSuggestionRequest.Title,
+                DeadLine = addSuggestionRequest.DeadLine,
+                Phase = addSuggestionRequest.Phase,
+                Description = addSuggestionRequest.Description,
+            };
+            
+                await dbContext.Suggestions.AddAsync(suggestion);
+                await dbContext.SaveChangesAsync();
+                return Ok(suggestion);
+
+            
+        }
+
+        [HttpDelete]
+        [Route("/Delete/Suggestion/{id:int}")]
+        public async Task<IActionResult> DeleteSuggestionById([FromRoute] int id)
+        {
+            var suggestion = await dbContext.Suggestions.FindAsync(id);
+
+            if (suggestion != null)
+            {
+                dbContext.Remove(suggestion);
+                await dbContext.SaveChangesAsync();
+                return Ok();
+
+            }
+            return NotFound();
+        }
+    }
 }
-}
+
---
+++ Shared/Models/API/AddSuggestionRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Nordic_Door.Shared.Models.API
+{
+    public class AddSuggestionRequest
+    {
+        public int Id { get; }
+
+        public int TeamId { get; set; }
+
+        public int? ResponsibleEmployee { get; set; }
+
+        public int? ResponsibleTeam { get; set; }
+
+        public int CreatedBy { get; set; } // Må fikse hvordan automatisk loade inn fra database(tror ikke det skal være med set;).
+
+        public string Title { get; set; }
+
+        public DateTime CreatedAt { get; }
+
+        public DateTime? DeadLine { get; set; }
+
+        public DateTime? LastUpdatedAt { get; }
+
+        public string? Phase { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
+
