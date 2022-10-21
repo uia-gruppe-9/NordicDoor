3666321d3a20c2e71bd69741e69ddefa92d35d80
Markus <Markusj@uia.no>
2022-10-21 17:14:14 +0200

    Lagt til Ferdig Events database, Lagt til Semi-Ferdig EventsController

---
+++ Server/Controllers/EventsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nordic_Door.Server.Data;
+using Nordic_Door.Shared.Models.API;
+using Nordic_Door.Shared.Models.Database;
+using static MudBlazor.Colors;
+
+
+namespace Nordic_Door.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class EventsController : Controller
+    {
+
+        public NordicDoorsDbContext dbContext { get; set; }
+
+        public EventsController(NordicDoorsDbContext ctx)
+        {
+            dbContext = ctx;
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetEvents()
+        {
+            var events = await dbContext.Events.ToListAsync();
+            return Ok(events);
+        }
+
+        //Lage funksjon som viser informasjon til spesifik ID (HttpGet)
+
+        [HttpGet]
+        [Route("/Search/EventsById{id:int}")]
+        public async Task<IActionResult> GetEventsById([FromRoute] int id)
+        {
+            var events = await dbContext.Events.FindAsync(id);
+
+            if (events == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(events);
+        }
+
+        //Funksjon som lager nye events(HttpPut)
+
+
+
+    }
+}
+
---
+++ Shared/Models/Database/Events.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nordic_Door.Shared.Models.Database
+{
+
+    public class Event
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("Event_ID")]
+
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("History")]
+        [Column("History_ID")]
+        public int HistoryId { get; set; }
+
+
+        [Required]
+        [ForeignKey("Employees")]
+        [Column("Employee_ID")]
+
+        public int EmployeeId { get; set; }
+        [Required]
+
+        // Hvis error se på Description
+        public string Description { get; set; }
+        [Required]
+        public DateTime Timestamp { get; set; }
+
+    }
+
+
+
+}
