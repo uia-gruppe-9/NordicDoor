3dcc14125394fae3dd86f8094a85c946ac475c99
GitFA96 <89194109+GitFA96@users.noreply.github.com>
2022-10-24 14:40:24 +0200

    Siste finish med database data, skal være i orden nå.

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -26,6 +26,7 @@ CREATE TABLE UserTeam (
                           Employee_ID int,
                           Team_ID int,
                           Role int NOT NULL,
+                          PRIMARY KEY (Employee_ID,Team_ID),
                           FOREIGN KEY (Employee_ID) REFERENCES Employees (Employee_ID),
                           FOREIGN KEY (Team_ID) REFERENCES Teams (Team_ID)
 );
@@ -64,79 +65,415 @@ CREATE TABLE Events (
 
 INSERT INTO Teams values (
                              1,
-                             'Salg'
+                             'Salg og Marked'
                          );
 
 INSERT INTO Teams values (
                              2,
-                             'Maling'
+                             'Produksjon'
                          );
 
 INSERT INTO Teams values (
                              3,
-                             'Konstruksjon'
+                             'Teknisk-Avdeling'
+                         );
+
+INSERT INTO Teams values (
+                             4,
+                             'Logistikk'
+                         );
+
+INSERT INTO Teams values (
+                             5,
+                             'Produksjonsutvikling'
+                         );
+
+INSERT INTO Teams values (
+                             6,
+                             'IT-Avdeling'
+                         );
+
+INSERT INTO Teams values (
+                             7,
+                             'Økonomi og finans'
+                         );
+
+INSERT INTO Teams values (
+                             8,
+                             'HR-Avdeling'
+                         );
+
+INSERT INTO Teams values (
+                             9,
+                             'Administrasjon'
+                         );
+
+INSERT INTO Teams values (
+                             10,
+                             'Quick lager'
                          );
 
 insert INTO Employees values (
                                  1,
-                                 'Ogundleif Rollgangvolden',
-                                 'ogro@nordicdoors.no',
+                                 'Ola Nordmann',
+                                 'OlaN@nordicdoors.no',
                                  '1234',
                                  0
                              );
 
 insert INTO Employees values (
                                  2,
-                                 'Ogundleif Rollgangvolden',
-                                 'ogro@nordicdoors.no',
+                                 'Kari Nordmann',
+                                 'KariN@nordicdoors.no',
                                  '1234',
                                  1
                              );
 
 insert INTO Employees values (
                                  3,
-                                 'Ogundleif Rollgangvolden',
-                                 'ogro@nordicdoors.no',
+                                 'John Johnson',
+                                 'JonJ@nordicdoors.no',
+                                 '1234',
+                                 2
+                             );
+
+insert INTO Employees values (
+                                 4,
+                                 'Morten Harket',
+                                 'MortHa@nordicdoors.no',
+                                 '1234',
+                                 1
+                             );
+
+insert INTO Employees values (
+                                 5,
+                                 'Egil Berntsen',
+                                 'EgBer@nordicdoors.no',
                                  '1234',
                                  0
                              );
 
+insert INTO Employees values (
+                                 6,
+                                 'Thor Magne Svendsen',
+                                 'THMS@nordicdoors.no',
+                                 '1234',
+                                 1
+                             );
+
+insert INTO Employees values (
+                                 7,
+                                 'Eskil Lie',
+                                 'EskLi@nordicdoors.no',
+                                 '1234',
+                                 1
+                             );
+
+insert INTO Employees values (
+                                 8,
+                                 'Katrine Amundsen',
+                                 'KatAm@nordicdoors.no',
+                                 '1234',
+                                 1
+                             );
+
+insert INTO Employees values (
+                                 9,
+                                 'Susanne Tyri',
+                                 'susT@nordicdoors.no',
+                                 '1234',
+                                 0
+                             );
+
+insert INTO Employees values (
+                                 10,
+                                 'Maria Andersen',
+                                 'MarA@nordicdoors.no',
+                                 '1234',
+                                 0
+                             );
+insert INTO Employees values (
+                                 11,
+                                 'Mia Ås',
+                                 'MiaAA@nordicdoors.no',
+                                 '1234',
+                                 2
+                             );
+insert INTO Employees values (
+                                 12,
+                                 'Espen Tømmerstigen',
+                                 'Espentom@nordicdoors.no',
+                                 '1234',
+                                 0
+                             );
+
+insert into UserTeam values (
+                                1,
+                                1,
+                                0
+                            );
+
+insert into UserTeam values (
+                                2,
+                                2,
+                                1
+                            );
+
+insert into UserTeam values (
+                                3,
+                                3,
+                                2
+                            );
+
+insert into UserTeam values (
+                                4,
+                                4,
+                                1
+                            );
+
+insert into UserTeam values (
+                                5,
+                                5,
+                                0
+                            );
+
+insert into UserTeam values (
+                                6,
+                                6,
+                                1
+                            );
+
+insert into UserTeam values (
+                                7,
+                                7,
+                                1
+                            );
+
+insert into UserTeam values (
+                                8,
+                                8,
+                                1
+                            );
+
+insert into UserTeam values (
+                                9,
+                                9,
+                                0
+                            );
+
+insert into UserTeam values (
+                                10,
+                                10,
+                                0
+                            );
+
+insert into UserTeam values (
+                                11,
+                                1,
+                                2
+                            );
+insert into UserTeam values (
+                                12,
+                                2,
+                                0
+                            );
+
 insert into Suggestions values ( default,
                                  1,
                                  1,
                                  1,
-                                 'Bedre mikrofoner',
+                                 'Bedre mikrofoner på Support',
                                  current_timestamp,
                                  1,
                                  current_timestamp,
                                  current_timestamp,
-                                 'new',
-                                 'plan',
-                                 'Nødvendiger fra kunder. ');
+                                 'New',
+                                 'Study',
+                                 'Tilbakemedling fra kunder. ');
 
 insert into Suggestions values ( default,
                                  2,
                                  2,
                                  2,
-                                 'tester data',
+                                 'Fellesrom: Nytt kjøleskap',
                                  current_timestamp,
                                  2,
+                                 current_timestamp,
+                                 current_timestamp,
+                                 'New',
+                                 'Do',
+                                 'Installere nytt kjøleskap på fellesrom. ');
+
+insert into Suggestions values ( default,
+                                 3,
+                                 3,
+                                 3,
+                                 'Bestille Flere spikere av lengde 2cm',
+                                 current_timestamp,
+                                 3,
                                  current_timestamp,
                                  current_timestamp,
                                  'pending',
                                  'do',
-                                 'Teste data teskt. ');
+                                 'Trenger for å fullføre dør. ');
 
 insert into Suggestions values ( default,
-                                 3,
-                                 3,
-                                 3,
-                                 'tester data',
+                                 4,
+                                 4,
+                                 4,
+                                 'Bestille nytt Trelim',
                                  current_timestamp,
-                                 3,
+                                 4,
                                  current_timestamp,
                                  current_timestamp,
                                  'pending',
-                                 'do',
-                                 'Teste data teskt. ');
+                                 'Act',
+                                 'Tomme for trelim. ');
 
+insert into Suggestions values ( default,
+                                 5,
+                                 5,
+                                 5,
+                                 'Bytte karm',
+                                 current_timestamp,
+                                 5,
+                                 current_timestamp,
+                                 current_timestamp,
+                                 'pending',
+                                 'Planing',
+                                 'Splint i dørkarm, må gjøres på nytt. ');
+
+insert into Suggestions values ( default,
+                                 6,
+                                 6,
+                                 6,
+                                 'Kaste søppel',
+                                 current_timestamp,
+                                 6,
+                                 current_timestamp,
+                                 current_timestamp,
+                                 'pending',
+                                 'Do',
+                                 'Kaste søpla på fellesrom. ');
+
+insert into Suggestions values ( default,
+                                 7,
+                                 7,
+                                 7,
+                                 'Trenger ny spraylakker',
+                                 current_timestamp,
+                                 7,
+                                 current_timestamp,
+                                 current_timestamp,
+                                 'pending',
+                                 'Do',
+                                 'Spraylakker slutta å fungere. ');
+
+insert into Suggestions values ( default,
+                                 8,
+                                 8,
+                                 8,
+                                 'Bare 1 dorull igjen',
+                                 current_timestamp,
+                                 8,
+                                 current_timestamp,
+                                 current_timestamp,
+                                 'pending',
+                                 'Do',
+                                 'Trenger mer dopapir til do 2. ');
+
+insert into Suggestions values ( default,
+                                 9,
+                                 9,
+                                 9,
+                                 'Bestille nye skruer av slag "lengde:1.5cm"',
+                                 current_timestamp,
+                                 9,
+                                 current_timestamp,
+                                 current_timestamp,
+                                 'pending',
+                                 'Do',
+                                 'Trenger skruer til dørkarm. ');
+
+insert into Suggestions values ( default,
+                                 10,
+                                 10,
+                                 10,
+                                 'Fiks dør til kontor',
+                                 current_timestamp,
+                                 10,
+                                 current_timestamp,
+                                 current_timestamp,
+                                 'Closed',
+                                 'do',
+                                 'Dør til kontor vil ikke lukkes. ');
+
+insert into Events values (
+                            DEFAULT,
+                            1,
+                            1,
+                            'Kommentar: Får mikrofoner tilsendt.',
+                            current_timestamp );
+
+insert into Events values (
+                              DEFAULT,
+                              2,
+                              2,
+                              'Kommentar: Fant ingen feil i data tekst.',
+                              current_timestamp );
+
+insert into Events values (
+                              DEFAULT,
+                              3,
+                              3,
+                              'Kommentar: Spikere skal komme i løpet av noen få strakser.',
+                              current_timestamp );
+
+insert into Events values (
+                              DEFAULT,
+                              4,
+                              4,
+                              'Lukket Forbedringsforslag: Fant trelim gjemt.',
+                              current_timestamp );
+
+insert into Events values (
+                              DEFAULT,
+                              5,
+                              5,
+                              'Lukket Forbedringsforlag: Kunne sandpapires bort',
+                              current_timestamp );
+
+insert into Events values (
+                              DEFAULT,
+                              6,
+                              6,
+                              'Opprettet Forbedrinsforslag: Kaste søppel på fellesrom.',
+                              current_timestamp );
+
+insert into Events values (
+                              DEFAULT,
+                              7,
+                              7,
+                              'Kommentar: Fikk rensa den.',
+                              current_timestamp );
+
+insert into Events values (
+                              DEFAULT,
+                              8,
+                              8,
+                              'Lukket Forbedringsforslag: Kjøpte med dopapir i pausen.',
+                              current_timestamp );
+
+insert into Events values (
+                              DEFAULT,
+                              9,
+                              9,
+                              'Lukket Forbedringsforslag: Bestille nye skruer av slag "lengde:1.5cm"',
+                              current_timestamp );
+
+insert into Events values (
+                              DEFAULT,
+                              10,
+                              10,
+                              'Opprettet Forbedringsforslag: Fiks dør til kontor.',
+                              current_timestamp );
