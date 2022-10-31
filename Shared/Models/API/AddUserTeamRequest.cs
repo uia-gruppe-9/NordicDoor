fc7e672927f724cf85cc721724f00d69ab2df03e
GitFA96 <89194109+GitFA96@users.noreply.github.com>
2022-10-31 14:19:33 +0100

    adding roles ++ improvements

---
+++ Shared/Models/API/AddUserTeamRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Nordic_Door.Shared.Models.Database;
+
+namespace Nordic_Door.Shared.Models.API
+{
+    public class AddUserTeamRequest
+    {
+        public int EmployeeId { get; set; }
+        public int TeamId { get; set; }
+        public string Role { get; set; }
+    }
+}
+
---
+++ Shared/Models/Database/EmployeeRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Nordic_Door.Shared.Models.Database
+{
+    public class EmployeeRole
+    {
+        [Key]
+        [Required]
+        [Column("EmpRole")]
+        public string Role { get; set; }
+    }
+}
+
