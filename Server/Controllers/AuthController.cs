b8b36f55dfbc1864874fc86f91792a484a672431
GitFA96 <89194109+GitFA96@users.noreply.github.com>
2022-11-02 16:35:57 +0100

    tryout av loging backend

---
+++ Server/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nordic_Door.Server.Data;
+using Nordic_Door.Shared.Models.Common;
+
+
+
+namespace NordicDoor.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+
+        public NordicDoorsDbContext dbContext { get; set; }
+
+        public AuthController(NordicDoorsDbContext ctx)
+        {
+            dbContext = ctx;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AuthUsernameAndPassword(string email, string password)
+        {
+            var user = await dbContext.Employees.SingleAsync(e => e.Email == email && e.Password == password);
+
+           if (user != null)
+           {
+
+                var userUserteam = await dbContext.UserTeams.Where(e => e.EmployeeId == user.Id).ToListAsync();
+
+                var teamRelations = new List<UserTeamRelation>();
+                foreach (var userteam in userUserteam)
+                {
+
+                    var team = await dbContext.Teams.FindAsync(userteam.TeamId);
+                    var userRole = userteam.Role;
+
+                    teamRelations.Add(new UserTeamRelation(
+                    ){
+                        UserRole = userRole,
+                        Team = team
+                    });
+                }
+
+
+                return Ok(teamRelations);
+            }
+
+            return StatusCode(500);
+        }
+    }
+}
+
---
+++ Shared/Models/Common/UserTeamRelation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Nordic_Door.Shared.Models.Database;
+namespace Nordic_Door.Shared.Models.Common
+{
+    public class UserTeamRelation
+    {
+
+       public Team Team { get; set; }
+        public string UserRole { get; set; }
+
+
+    }
+}
+
