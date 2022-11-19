f0c5102a6a7ccb5c6c8b4caf90917dc6c4b42d85
Markus <markusj@uia.no>
2022-11-19 16:17:44 +0100

    Pusher opp så en broder kan få hjelp på live share

---
+++ Client/Models/GetTeamStatistics.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Nordic_Door.Shared.Models.Database;
+
+namespace Nordic_Door.Client.Models {
+
+    public class GetTeamStatistics {
+
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Status { get; set; }
+
+
+
+
+    }
+}
