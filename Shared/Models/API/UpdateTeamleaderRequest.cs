7a8bb96b7c58a0eb95801f3327b0ae0c0fa5447f
Tobias Kroknes Kristiansen <tobias.kristiansen@norsktitanium.com>
2022-11-25 20:25:51 +0100

    Fiks comment refresh og teams

--- Shared/Models/API/UpdateTeamleaderRequest.cs
+++ Shared/Models/API/UpdateTeamleaderRequest.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Nordic_Door.Shared.Models.API
 {
 	public class UpdateTeamLeaderRequest
 	{
-		public string employeeName { get; set; }
-		public string teamName { get; set; }
-		
-	}
-}
 
+            public int EmployeeId { get; set; }
+            public List<string> TeamNames { get; set; }
+
+    
+    }
+}
